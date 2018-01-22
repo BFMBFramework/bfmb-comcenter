8d7ebef72be3662d4a13510cb1fb6a83e3deb8d4
Ángel González <aglezabad@gmail.com>
2018-01-22 16:42:30 +0100

    First successful socket test

--- server.js
+++ server.js
@@ -1,36 +1,14 @@
 const net = require('net')
-const winston = require('winston')
 
+const serverEvents = require('./lib/serverEvents.js')
+const logger = require('./lib/logger.js').logger
+
+// Requiring configuration file
 const config = require('./config.json')
 
-// Configuring logger
-const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)(),
-    new (winston.transports.File)({filename: 'comcenter.log'})
-  ]
-})
-
+// Creating server
 const server = net.createServer()
-server.listen(config.network.port, config.network.host, startCallback)
-
-server.on('connection', function (sock) {
-  // We have a connection - a socket object is assigned to the connection automatically
-  logger.info('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort)
-
-  // Add a 'data' event handler to this instance of socket
-  sock.on('data', function (data) {
-    logger.info('DATA ' + sock.remoteAddress + ': ' + data)
-    // Write the data back to the socket, the client will receive it as data from the server
-    sock.write('You said "' + data + '"')
-  })
-
-  // Add a 'close' event handler to this instance of socket
-  sock.on('close', function (data) {
-    logger.info('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort)
-  })
-})
-
-function startCallback () {
+server.listen(config.network.port, config.network.host, function () {
   logger.info('Server listening on ' + server.address().address + ':' + server.address().port)
-}
+})
+server.on('connection', serverEvents.clientConnected)
