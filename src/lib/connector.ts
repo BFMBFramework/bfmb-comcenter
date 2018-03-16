6ecd8e27cea2790a3a3767e709cd1b8f9502a640
Ángel González <aglezabad@gmail.com>
2018-03-16 12:59:05 +0100

    Added abstract Connector/Connection class

--- src/lib/connector.ts
+++ src/lib/connector.ts
@@ -1,3 +1,5 @@
+import * as uuidv1 from "uuid/v1";
+
 import {config} from "./config";
 
 export class ConnectorManager {
@@ -18,5 +20,59 @@ export class ConnectorManager {
 		}
 
 		return null;
+	}
+}
+
+export abstract class Connector {
+	private name : string;
+	private connections : Array<Connection>;
+
+	constructor(name : string) {
+		this.name = name;
+	}
+
+	getName() : string {
+		return this.name;
+	}
+
+	abstract addConnection(options : any, callback : Function) : void;
+
+	getConnectionIndex(id : string) : number {
+		return this.connections.findIndex((i : Connection) => { return i.getId() === id});
+	}
+	
+	getConnection(id : string) : Connection {
+		let index : number = this.getConnectionIndex(id);
+		if (index > -1) {
+			return this.connections[index];
+		} else {
+			return null;
+		}
+	}
+
+	removeConnection(id : string, callback : Function) : void {
+		let index : number = this.getConnectionIndex(id);
+		if (index > -1) {
+			this.connections.splice(index, 1);
+			callback(null);
+		} else {
+			callback(new Error("No connection on list with id: " + id));
+		}
+	}
+
+	abstract receiveMessage(id : string, options : any, callback : Function) : void;
+
+	abstract sendMessage(id : string, options : any, callback : Function) : void;
+}
+
+export abstract class Connection {
+	private id : string;
+
+	constructor (token : string) {
+		this.id = uuidv1();
+	}
+
+	getId() : string {
+		return this.id;
 	}
 }
